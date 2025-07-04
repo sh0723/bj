af3d42cd5ac29de1918696489e3e02f1bc19c930
sh0723 <pshpark12345@naver.com>
2025-07-04 15:24:07 +0900

    solve : 16139 인간-컴퓨터 상호작용

--- 16139/bj16139.cpp
+++ 16139/bj16139.cpp
@@ -1,61 +1,65 @@
-#include <iostream>
-#include <vector>
-#include <tuple>
+    #include <iostream>
+    #include <vector>
+    #include <tuple>
 
-using namespace std;
+    using namespace std;
 
-int main() {
+    int main() {
 
-    string s;
-    cin >> s;
+        string s;
+        cin >> s;
 
-    vector<char> input_string;
+        vector<char> input_string;
 
-    for (char c : s) {
-        input_string.push_back(c);
-    }
-    int string_size = input_string.size();
-    int n;
-    vector<tuple<int,int,char>> vector_to_output(n);
+        for (char c : s) {
+            input_string.push_back(c);
+        }
+        int string_size = input_string.size();
+        int n;
+        cin >> n;
 
-    char target;
-    int st_index, end_index;
+        vector<tuple<int,int,char>> vector_to_output(n);
 
-    vector<vector<int>> count('z'-'a' + 1);
+        char target;
+        int st_index, end_index;
 
-    for (int i=0; i<n; i++) {
-        cin >> target >> st_index >> end_index;
-        vector_to_output[i] = make_tuple(st_index, end_index, target);
-        if (count[target-'a'].size() > 0)
-            continue;
+        vector<vector<int>> count('z'-'a' + 1, vector<int>(string_size, 0));
 
-        count[target-'a'].resize(string_size,0);
-
-        if (input_string[0] == target) {
-            count[target-'a'][0] = 1;
-        } else {
-            count[target-'a'][0] = 0;
+        for (int i=0; i<n; i++) {
+            cin >> target >> st_index >> end_index;
+            vector_to_output[i] = make_tuple(st_index, end_index, target);
         }
 
-        for (int j=1; j<string_size; j++) {
-
-            if (input_string[j] == target) {
-                count[target-'a'][j] = count[target-'a'][j-1] + 1;
-            } else {
-                count[target-'a'][j] = count[target-'a'][j-1];
+        for (char c='a'; c<='z'; c++) {
+            if (input_string[0] == c) {
+                count[c-'a'][0] = 1;
+            }
+            for (int i=1; i<string_size; i++) {
+                if (input_string[i] == c) {
+                    count[c-'a'][i] = count[c-'a'][i-1] + 1;
+                } else {
+                    count[c-'a'][i] = count[c-'a'][i-1];
+                }
             }
         }
+
+        for (int i=0; i<n; i++) {
+            target = get<2>(vector_to_output[i]);
+            st_index = get<0>(vector_to_output[i]);
+            end_index = get<1>(vector_to_output[i]);
+
+            if (st_index == 0) {
+                cout << count[target-'a'][end_index] << '\n';
+            } else {
+                cout << count[target-'a'][end_index] - count[target-'a'][st_index-1] << '\n';
+            }
+        }
+
+
+
+        return 0;
     }
 
-    for (int i=0; i<n; i++) {
-        target = get<2>(vector_to_output[i]);
-        st_index = get<0>(vector_to_output[i]);
-        end_index = get<1>(vector_to_output[i]);
-
-        cout << count[target-'a'][end_index] - count[target-'a'][st_index] << endl;
-    }
-
-
-
-    return 0;
-}
+/*
+ 미리 a~z에 해당하는 누적개수에 대한 vector를 만들어놓음으로써 시간을 단축시킴.
+ */
